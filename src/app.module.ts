9ec8a907212c9e7ad6bf05026664619f44e01e90
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2023-11-23 11:40:11 +1100

    adds mikro-orm config and a basic user seeder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './app/user/user.module';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
-  imports: [],
+  imports: [MikroOrmModule.forRoot(), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
