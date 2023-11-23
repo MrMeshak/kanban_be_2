1d28b28ca43fd91193ce40946c6605fc6f1e5d85
MrMeshak <94204153+MrMeshak@users.noreply.github.com>
2023-11-23 12:41:45 +1100

    adds redis module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './app/user/user.module';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [MikroOrmModule.forRoot(), UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MikroOrmModule.forRoot(),
+    UserModule,
+    RedisModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
